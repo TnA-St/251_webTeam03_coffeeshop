e585785fe787077b247031ee7ff0ed8192d9021c
THAI6868 <thai.2474802010354@vanlanguni.vn>
2025-11-14 10:32:38 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/config.php';
+if (!isset($_GET['id'])) { header('Location: index.php'); exit; }
+$id = (int) $_GET['id'];
+$stmt = $pdo->prepare('DELETE FROM products WHERE id = ?');
+$stmt->execute([$id]);
+header('Location: index.php'); exit;
+?>
